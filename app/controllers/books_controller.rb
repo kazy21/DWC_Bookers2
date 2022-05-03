c3e026338c4179986d0544ccaa9590eddd96dfd5
EC2 Default User <ec2-user@ip-172-31-8-1.ec2.internal>
2022-05-03 09:11:31 +0000

    [finish]44行目

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,14 @@
 class BooksController < ApplicationController
   def index
-    @book = Book.new
+    @new_book = Book.new
     @books = Book.all
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
-      redirect_to books_path
+    @new_book = Book.new(book_params)
+    @new_book.user_id = current_user.id
+    if @new_book.save
+      redirect_to books_path, notice: 'You have created book successfully.'
     else
       @books = Book.all
       render :index
@@ -16,10 +16,27 @@ class BooksController < ApplicationController
   end
 
   def show
+    @new_book = Book.new
     @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path, notice: 'You have updated book successfully.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,33 @@
 class UsersController < ApplicationController
   def index
+    @new_book = Book.new
+    @users = User.all
   end
 
   def show
+    @new_book = Book.new
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user.id != current_user.id
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'You have updated user successfully.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
